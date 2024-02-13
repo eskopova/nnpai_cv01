9df2508c8aa160c55af77e27399342039619917a
Eva Skopova <st72711@upce.cz>
2024-02-13 17:59:19 +0100

    application context

--- src/main/java/com/example/cv01/Application.java
+++ src/main/java/com/example/cv01/Application.java
@@ -2,12 +2,16 @@ package com.example.cv01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application extends Greeter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ApplicationContext context = SpringApplication.run(Application.class, args);
+
+		MyController myController = context.getBean(MyController.class);
+		System.out.println(myController.sayHello());
 	}
 
 }
